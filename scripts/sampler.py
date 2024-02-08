0af2a128ec5c8db72b7d1171a0854daec1b1c6e2
BugraAlptekinSari <bugraalptekinsari@gmail.com>
2024-02-09 00:32:00 +0300

    remove redundant line

--- scripts/sampler.py
+++ scripts/sampler.py
@@ -14,5 +14,3 @@ for i in arr:
        exit_status = os.system(f"mpirun --map-by core -np 16 ../build/softmax ../data/{i}.txt 1000000 500 1>> {out_file}.txt 2>> {out_file}_error.txt");
        if(exit_status != 0):
               os.system(f'echo "FAILED: {i} WITH: {exit_status}" >> test1_failures.txt')
-
-# mpirun --map-by core -np 16 ../build/src/inner ../matrices/{i}.mtx  ../parts/{i}{j}  1000 >> output3.txt
